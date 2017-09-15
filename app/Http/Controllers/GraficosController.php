79d07129e3bd066e0c3adcb6a365eddd851b282d
chantezan <pablo.poblete@ing.uchile.cl>
2017-09-15 01:07:53 -0300

    definitivo

--- app/Http/Controllers/GraficosController.php
+++ app/Http/Controllers/GraficosController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Seccion;
 use App\User;
 use Illuminate\Http\Request;
 use App\Curso;
@@ -12,37 +13,40 @@ class GraficosController extends Controller
 {
     public function index(Request $request){
 
-        $cursos = Curso::with(['tipo.preguntas','secciones.respuestas.pregunta','secciones.profesores','secciones.auxiliares','secciones.coordinadores'])->get();
-        foreach($cursos as $curso){
-            foreach ($curso->secciones as $seccion2){
-                $auxiliares = collect();
-                if($curso->id_tipo == 3) {
-                    foreach ($seccion2->curso->secciones as $seccion) {
-                        foreach ($seccion->auxiliares as $aux) {
-                            $auxiliares->push($aux);
-                        }
-                        foreach ($seccion->coordinadores as $aux) {
-                            $auxiliares->push($aux);
-                        }
-                    }
-                } else if($curso->id_tipo == 4) {
-                    foreach ($seccion2->curso->secciones as $seccion) {
-                        foreach ($seccion2->auxiliares as $aux) {
-                            $auxiliares->push($aux);
-                        }
-                        foreach ($seccion->coordinadores as $aux) {
-                            $auxiliares->push($aux);
-                        }
-                    }
-                } else {
-                    $auxiliares = $seccion2->auxiliares;
-                }
-                $auxiliares = $auxiliares->unique('id');
-                $seccion2->auxiliares_all = $auxiliares->toArray();
-            }
-        }
-        //dd($cursos->toArray());
+        $cursos = Curso::with(['tipo.preguntas','secciones.profesores','secciones.auxiliares','secciones.coordinadores'])->get();
+
         return view('welcome',['cursos' => $cursos]);
+    }
+
+    public function getSeccion(Request $request){
+        $seccion = Seccion::with(['auxiliares','profesores','coordinadores','respuestas','curso'])->find($request['seccion']);
+        foreach ($seccion->curso->secciones as $seccion2){
+            $auxiliares = collect();
+            if($seccion->curso->id_tipo == 3) {
+                foreach ($seccion2->curso->secciones as $seccion) {
+                    foreach ($seccion->auxiliares as $aux) {
+                        $auxiliares->push($aux);
+                    }
+                    foreach ($seccion->coordinadores as $aux) {
+                        $auxiliares->push($aux);
+                    }
+                }
+            } else if($seccion->curso->id_tipo == 4) {
+                foreach ($seccion2->curso->secciones as $seccion) {
+                    foreach ($seccion2->auxiliares as $aux) {
+                        $auxiliares->push($aux);
+                    }
+                    foreach ($seccion->coordinadores as $aux) {
+                        $auxiliares->push($aux);
+                    }
+                }
+            } else {
+                $auxiliares = $seccion2->auxiliares;
+            }
+            $auxiliares = $auxiliares->unique('id');
+            $seccion->auxiliares_all = $auxiliares->toArray();
+        }
+        return $seccion;
     }
 
     public function logeado(){
@@ -54,7 +58,7 @@ class GraficosController extends Controller
 
         $cursos = collect();
         foreach ($usuario->secciones as $seccion)
-            $cursos->push(Curso::with(['tipo.preguntas','secciones.respuestas.pregunta','secciones.profesores','secciones.auxiliares','secciones.coordinadores'])
+            $cursos->push(Curso::with(['tipo.preguntas','secciones.profesores','secciones.auxiliares','secciones.coordinadores'])
                 ->find($seccion->id_curso));
         $cursos = $cursos->unique('id');
         foreach($cursos as $curso){
